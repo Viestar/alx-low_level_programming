c043bb86b52c04fa4da91d6c1f464cbc8389fb3e
Viestar <m.silverbuckson@gmail.com>
2023-02-16 12:21:21 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
 
-/*  Using puts to output a string */
+/*
+* Return 0
+* Using puts to output a string
+*/
 int main(void)
 {
-      char hello[70] = "\"Programming is like building a multilingual puzzle";
+char hello[70] = "\"Programming is like building a multilingual puzzle";
 
-      puts(hello);
+puts(hello);
 
-      return (0);
+return (0);
 }
