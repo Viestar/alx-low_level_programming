bdc4f3cdfa02a9419fcb2cd9bd7d0fb5419caba2
Viestar <m.silverbuckson@gmail.com>
2023-02-16 13:39:53 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,11 @@
-#include <stdio>
+#include <stdio.h>
+/**
+*main - Uses write to output
+*Return: 1 is returned
+*/
+
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+return (1);
+}
