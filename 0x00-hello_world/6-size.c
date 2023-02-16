e73754d15870dc6e772c32b0d37ace8993b42fae
Viestar <m.silverbuckson@gmail.com>
2023-02-16 13:02:11 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,23 @@
-#include <stdio>
+#include <stdio.h>
+
+/**
+* main - Program entry
+* Return: 0 for success and 1 for error
+*/
+
+int main(void)
+{
+char c;
+int i;
+long int li;
+long long int lli;
+float f;
+
+printf(" Size of a Char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf(" Size of an int: %lu byte(s)\n", (unsigned long)sizeof(1));
+printf(" Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+printf(" Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
+printf(" Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+
+return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -7,3 +7,4 @@
 * 3-name ---- compiles a C file and creates an executable named cisfun
 * 4-puts.c ---  C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
 * 5-printc --- Uses printf to output "with proper grammar, but the outcome is a piece of art,
+* 6-size --  C program that prints the size of various types on the computer it is compiled and run on
