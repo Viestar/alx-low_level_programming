675911472515596a5c83f6849aaf48558fab0b26
Viestar <m.silverbuckson@gmail.com>
2023-02-21 12:34:49 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,18 @@
-#include <unistd.h>
 #include <stdio.h>
-
 
 /**
  *_isalpha -- Check whether an argument passed is an alphabet.
+ *@c: Argument to be passed
  *Return: 1 when when lower and 0 otherwise 0
  */
 
 int _isalpha(int c)
 {
-if (islower(c) || isupper(c))
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else if (c >= 'A' && c <= 'Z')
 {
 return (1);
 }
