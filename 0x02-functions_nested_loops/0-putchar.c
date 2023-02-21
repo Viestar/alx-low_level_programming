c1ed0434d0c9fff48efae355a0e0bf237e1eba3d
Viestar <m.silverbuckson@gmail.com>
2023-02-21 06:46:33 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,12 +7,13 @@
 
 int main(void)
 {
-char word[10] = "_putchar\n";
+char word[8] = "_putchar";
 int one = sizeof(word);
 int i;
 for (i = 0; i <= one; i++)
 {
 _putchar(word[i]);
 }
+_putchar('\n');
 return (0);
 }
