7cbb3572310e3a05960b94ee356e9183dba72823
Viestar <m.silverbuckson@gmail.com>
2023-02-21 11:32:23 -0800

    3-islower.c final

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,21 +1,25 @@
-#include <unistd.h>
-#include <stdio.h>
+#include "main.h"
 
 
 /**
  *_islower -- Check whether an argument passed is lower
+ *@c: the argument received
  *Return: 1 when when lower and 0 otherwise 0
  */
 
 int _islower(int c)
 {
-  if (islower(c))
-    {
-return (1);
+char lchar;
 
-    }
-  else
-    {
+for (lchar = 'a'; lcahr <= 'z'; lcahr++)
+{
+if (c == lchar)
+{
+return (1);
+}
+else
+{
 return (0);
+}
 }
 }
