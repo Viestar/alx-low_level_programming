f20df1e17d2fcb6c77f169acdf05dd666750448a
Viestar <m.silverbuckson@gmail.com>
2023-02-21 06:39:42 -0800

    My first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Main function here
+ * Return: Always 0 when successful
+ */
+
+int main(void)
+{
+char word[10] = "_putchar\n";
+int one = sizeof(word);
+int i;
+for (i = 0; i <= one; i++)
+{
+_putchar(word[i]);
+}
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+# 0x02-functions_nested_loops
+* 0-putchar.c --- Outputting a string without using the standard library i.e using a self defined defined library
