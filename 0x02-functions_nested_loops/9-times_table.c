67aa285d83b31092b698faac0d7c7ab7d36a7cb5
Viestar <m.silverbuckson@gmail.com>
2023-02-21 15:27:16 -0800

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * times_table - Prints time table
+ * Return: Void
+ */
+
+void times_table(void)
+{
+int col, row, multiple, ones, tens;
+
+for (row = 0; row <= 9; row++)
+{
+for (col = 0; col <= 9; col++)
+{
+multiple = col * row;
+tens = multiple / 10;
+ones = multiple % 10;
+if (col == 0)
+{
+_putchar('0');
+}
+else if (mulitple < 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(ones + '0');
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+_putchar(tens + '0');
+_putchar(ones + '0');
+}
+}
+_putchar('\n');
+}
+}
