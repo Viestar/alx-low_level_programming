1a38a9f24a55f10880bf7cb510e040f15697fb30
Viestar <m.silverbuckson@gmail.com>
2023-02-21 12:49:43 -0800

    5-sign.c final

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *print_sign -- Check whether an argument passed is an alphabet.
+ *@n: Argument to be passed
+ *Return: 1 when positive, -1 when negative &  0 when 0
+ */
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+';
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+_putchar('-');
+return (-1);
+}
