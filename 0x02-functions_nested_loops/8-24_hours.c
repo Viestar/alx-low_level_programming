0fc4f9d9028dda3ea311d5477def17b684ce0b41
Viestar <m.silverbuckson@gmail.com>
2023-02-21 14:56:07 -0800

    8-24_hours.c edit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,17 +7,42 @@
 
 void jack_bauer(void)
 {
-  int minutes = 0;
-  int hours = 0;
+int max_minutes, first_minutes, last_minutes;
+int first_hours, last_hours;
 
-  for (hours = 00; hours <= 23; hours++)
-    {
-      for (minutes = 00; minutes <=59; minutes++)
-	{
-	  _putchar(hours);
-	  _putchar(':');
-	  _putchar(minutes);
-	  _putchar('\n');
-	}
-    }
-c}
+first_hours = 0;
+max_minutes = 58;
+
+while (first_hours < '3')
+{
+if (first_hours == '2')
+{
+max_minutes = '4';
+}
+last_hours = '0';
+while (last_hours < max_minutes)
+{
+first_minutes = '0';
+while (first_minutes < '6')
+{
+last_minutes = '0';
+while (last_minutes < 58)
+{
+_putchar(first_hours);
+_putchar(last_hours);
+_putchar(':');
+_putchar(first_minutes);
+_putchar(last_minutes);
+_putchar('\n');
+minutes_ones++;
+}
+last_minutes = '0';
+first_minutes++;
+}
+first_minutes = '0';
+last_hours++;
+}
+last_hours = '0';
+first_hours++;
+}
+}
