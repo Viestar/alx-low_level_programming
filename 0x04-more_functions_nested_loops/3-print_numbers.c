ee29cd54385ef0e2a9d783c898cec9bcd9cd3722
viestar <m.silverbuckson@gmail.com>
2023-02-23 14:34:54 +0300

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers  0 to 9
+ * Return: Void
+ */
+
+void print_numbers(void)
+{
+char x = 0;
+
+while (x <= 9)
+{
+_putchar(x + '0');
+a++;
+}
+_putchar('\n');
+}
