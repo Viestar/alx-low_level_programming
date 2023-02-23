f2330bfac1bb3df0443284d614c5bd7043418025
Viestar <m.silverbuckson@gmail.com>
2023-02-23 02:40:49 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *mul - multiplies 2 givemnn arguments
+ *@a: first integer to be multipliedjj,bbbbbbbbbbb
+ *@b: second integer to be multoto ghg
+ * Return: the produc
+ */
+
+int mul(int a, int b)
+{
+int product;
+
+product = a * b;
+return (product);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -2,3 +2,4 @@
  ## MOre practice examples with functions and nested loops
 * 1_isupper.c --- a function that checks for uppercase character
 * 1-isdigit.c -- function checks whether a given input is digit
+* 2-mul.c -- function multiplies the 2 givn arguments
