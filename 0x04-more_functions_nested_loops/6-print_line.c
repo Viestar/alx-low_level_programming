32352cc2d880544ac3239d018bdadd9f3e507209
Viestar <m.silverbuckson@gmail.com>
2023-02-23 17:15:16 +0300

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,24 +3,17 @@
 /**
  * print_line - prints underscore n times
  * @n: variable
- * Return: 0
+  * Return: void
  */
 
 void print_line(int n)
 {
 int i = 0;
 
-if (n == 0)
-{
-_putchar('\n');
-}
-else
-{
-while (i <= n)
+while (i < n && n > 0)
 {
 _putchar('_');
 i++;
 }
 _putchar('\n');
-}
 }
