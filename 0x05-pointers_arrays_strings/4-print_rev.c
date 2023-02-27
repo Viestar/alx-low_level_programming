bfcf4ed58c409b7b892b384f227c5a03f3434c72
Viestar <m.silverbuckson@gmail.com>
2023-02-27 18:21:17 +0300

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+/**
+* print_rev - Prints a given string
+* @s: A string pointer whose value to be printed.
+* Return: Void
+*/
+
+void print_rev(char *s)
+{
+int index = 0;
+int i;
+
+while (s[index])
+{
+index++;
+}
+for (i = index; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+
+}
+
