9d369add5bf102cf3be073698f84f2be551cd574
Viestar <m.silverbuckson@gmail.com>
2023-02-27 15:49:15 +0300

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+
+/**
+* reset_to_98 - Updates value of a variable
+* @n: pointer  of the varibale to be assigned.
+* Return: Void
+*/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+# 0x05. C - Pointers, arrays and strings
+
+* **98 Battery st. ./0-reset_to_98.c ---- Function that takes a pointer to an int as parameter and updates the value it points to 98
