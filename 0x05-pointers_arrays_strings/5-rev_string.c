5b3a432b4c1115f0d5adb20645ed8b290b86f076
Viestar <m.silverbuckson@gmail.com>
2023-02-28 11:29:57 +0300

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,17 +9,21 @@
 
 void rev_string(char *s)
 {
-int len = 0;
-int index;
+int len = 0, count = 0;
 char temp;
 
-while (s[len])
-len++;
-for (index = 0; index <= (len / 2); index++)
+while (*s)
 {
-temp = s[index];
-s[index]  = s[len - index - 1];
-s[len - index - 1] = temp;
-
+count++; /* find length of the string */
+s++;
+}
+for (len = 0; len < count; len++)
+s--;
+for (len = 0; len < (count / 2); len++)
+{
+int index = count;
+temp = s[index - len - 1];
+s[index - len - 1] = s[len];
+s[len] = temp;
 }
 }
