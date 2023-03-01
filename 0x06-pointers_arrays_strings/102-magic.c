b50236d49e9448acc89ce9781266db972a8dc1da
Viestar <m.silverbuckson@gmail.com>
2023-03-02 00:00:58 +0300

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,13 +2,13 @@
 
 int main(void)
 {
-  int n;
-  int a[5];
-  int *p;
+int n;
+int a[5];
+int *p;
 
-  a[2] = 1024;
-  p = &n;
-  *(5 + p) = 98;
-  printf("a[2] = %d\n", a[2]);
-  return (0);
+a[2] = 1024;
+p = &n;
+*(5 + p) = 98;
+printf("a[2] = %d\n", a[2]);
+return (0);
 }
