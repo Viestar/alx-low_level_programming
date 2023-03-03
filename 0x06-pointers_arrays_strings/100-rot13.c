3ba67e74581f1199d2d38c92c692dc8a52ab3a9f
Viestar <m.silverbuckson@gmail.com>
2023-03-03 09:04:50 +0300

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,26 +8,43 @@
 
 char *rot13(char *str)
 {
-	int index = 0;
+	int count_one = 0;
+	int count_two;
 
-	while (str[index])
+	char encoded_rot13[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+			     'T', 'U', 'V', 'W', 'X', 'Y',
+			     'Z', 'A', 'B', 'C', 'D', 'E',
+			     'F', 'G', 'H', 'I', 'J', 'K',
+			     'L', 'M', 'n', 'o', 'p', 'q',
+			     'r', 's', 't', 'u', 'v', 'w',
+			     'x', 'y', 'z', 'a', 'b', 'c',
+			     'd', 'e', 'f', 'g', 'h', 'i',
+			     'j', 'k', 'l', 'm'};
+
+	char normal_alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+			     'G', 'H', 'I', 'J', 'K', 'L',
+			     'M', 'N', 'O', 'P', 'Q', 'R',
+			     'S', 'T', 'U', 'V', 'W', 'X',
+			     'Y', 'Z', 'a', 'b', 'c', 'd',
+			     'e', 'f', 'g', 'h', 'i', 'j',
+			     'k', 'l', 'm', 'n', 'o', 'p',
+			     'q', 'r', 's', 't', 'u', 'v',
+			     'w', 'x', 'y', 'z'};
+
+
+	while (str[count_one])
 	{
-
-		while ((str[index] <= 'M' || str[index] <= 'm')
-			&& (str[index] >= 'A' || str[index] >= 'a'))
+		for (count_two = 0; count_two <= 51; count_two++)
 		{
-			str[index] += 13;
-			index++;
+			if (str[count_one] == normal_alphabet[count_two])
+			{
+				str[count_one] = encoded_rot13[count_two];
+				break;
+			}
 		}
 
-		if ((str[index] >= 'N' || str[index] >= 'n')
-			&& (str[index] <= 'Z' || str[index] <= 'z'))
-		{
-			str[index] -= 13;
-		}
-		else
-			index++;
-		index++;
+		count_one++;
+
 	}
 	return (str);
 }
