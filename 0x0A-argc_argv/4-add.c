f2eb4b1b1a2df7ee06b23d9c3217354e60cd6d25
Viestar <m.silverbuckson@gmail.com>
2023-03-10 20:51:27 +0300

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,36 +12,33 @@
 
 int main(int argc, char *argv[])
 {
-	int index, sum;
+int index, sum;
 
-	sum = index = 0;
+sum = index = 0;
 
-	/* When no numbers passed */
-	if (argc == 1)
-	{
-		printf("0\n");
-	}
-	else
-	{
-		/* Iterating through all passed arguments except the first*/
-		for (index = 1; index < argc; index++)
-		{
-			/* Using atoi() to check if argc at argv[index] is an integer */
-			if (atoi(argv[index]))
-			{
-			/* Using the atoi() function to turn a string an integer */
-			    sum = sum + atoi(argv[index]);
-			}
-			else
-			{
-				/* Output for non integers*/
-				printf("Error\n");
-				return (1); /* Returning 1 if unsuccessful */
-			}
-		}
-		/*Finally printing the sum */
-		printf("%d\n", sum);
-	}
-
-	return (0); /* Returning 0 for success */
+/* When no numbers passed */
+if (argc == 1)
+{
+printf("0\n");
+}
+else
+{
+/* Iterating through all passed arguments except the first*/
+for (index = 1; index < argc; index++)
+{
+/* Using atoi() to check if argc at argv[index] is an integer */
+if (atoi(argv[index]))
+/* Using the atoi() function to turn a string an integer */
+sum = sum + atoi(argv[index]);
+else
+{
+/* Output for non integers*/
+printf("Error\n");
+return (1); /* Returning 1 if unsuccessful */
+}
+}
+/*Finally printing the sum */
+printf("%d\n", sum);
+}
+return (0); /* Returning 0 for success */
 }
