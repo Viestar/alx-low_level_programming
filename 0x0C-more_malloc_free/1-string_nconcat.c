b0f726bd7cc53cf5f0c6eaa66a91fc232ede8815
Viestar <m.silverbuckson@gmail.com>
2023-03-17 20:57:50 +0300

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,29 +12,22 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *bonded_string; /* Declaring container for the two strings */
-	unsigned int length_s2, total_length, length_s1, index, count;
+	unsigned int length_s2, length_s1, index, count;
 
-	length_s2 = length_s1 = index = total_length = count = 0;
+	length_s2 = length_s1 = index = count = 0;
 	if (s1 == NULL) /* Checking if s1 is empty */
 		s1 = "";
 
 	if (s2 == NULL) /* Checking if s2 is empty */
 		s2 = "";
 
-	/* Looping through s2 to catch how many items to add to s1 */
-	while (s2[index] && index <= n)
-	{
-		length_s1++;
-		index++;
-	}
 	/* looping through s1 to get full size of new string*/
 	for (index = 0; s1[index]; index++)
 		length_s2++;
 
-	total_length = length_s2 + length_s1;
 
 	/* Allocating memory for the new string */
-	bonded_string = malloc(sizeof(char) * (total_length));
+	bonded_string = malloc(sizeof(char) * (length_s2 + 1));
 
 	/* Checking if any memory was created */
 	if (bonded_string == NULL)
@@ -42,12 +35,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	/* Copying contents of s1 into the new string */
 	for (index = 0; s1[index] != '\0'; index++)
-		bonded_string[index] = s1[index];
+		bonded_string[length_s1++] = s1[index];
 	/* Copying contents of s2 */
 	for (count = 0; s2[count] != '\0' && count < n; count++)
-		bonded_string[index + count] = s2[count];
+		bonded_string[length_s1++] = s2[count];
 	/* Adding Null to the end of the string */
-	bonded_string[total_length] = '\0';
+	bonded_string[length_s1] = '\0';
 
 	return (bonded_string);
 }
