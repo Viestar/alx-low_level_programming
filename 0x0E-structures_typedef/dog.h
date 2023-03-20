9c43ec94b1a24d67a8f82e060654170f45588b28
Viestar <m.silverbuckson@gmail.com>
2023-03-20 22:56:50 +0300

    dog.h number_3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -27,4 +27,12 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
 #endif /* DOG_H */
