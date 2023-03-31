72cd7f0cfb1413e45dc265da03e8d17c366be34d
Viestar <m.silverbuckson@gmail.com>
2023-03-31 13:17:20 +0300

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+void __attribute__((constructor)) tortoise_talk(void);
+
+/**
+ * tortoise_talk - Prints a string before the main.
+ */
+
+void tortoise_talk(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
