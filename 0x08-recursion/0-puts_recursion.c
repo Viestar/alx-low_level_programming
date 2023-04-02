b11994dbccefb3d098a86c5e14927e7d95dca914
Sylivester Mberenge <s.schmitzerx@gmail.com>
2023-04-02 08:06:33 +0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,9 +9,14 @@ void _puts_recursion(char *s)
 {
 	if (*s)
 	{
+  /* Printing a character to stdio */
 		_putchar(*s);
+  
+  /* function calling itself to perform same thing. 
 		_puts_recursion(s + 1);
 	}
+
 	else
+ /* Printing a new line incase base case is reached. 
 		_putchar('\n');
 }
