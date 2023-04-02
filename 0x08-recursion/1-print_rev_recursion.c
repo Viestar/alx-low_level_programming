f563bb3af9039732df67e3797ccad12092c95bb5
Sylivester Mberenge <s.schmitzerx@gmail.com>
2023-04-02 08:18:40 +0300

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,9 +7,13 @@
 
 void _print_rev_recursion(char *s)
 {
+ /* Cheking to see if s is not NULL */
 	if (*s)
 	{
+  /* Function calling its self with changing arguments - Recursion. */
 		_print_rev_recursion(s + 1);
+
+  /* Printing after recursing suchthat values get printed using LIFO */
 		_putchar(*s);
 	}
 }
