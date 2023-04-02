9e1941a793466634bc034084fbe589e9ce4b4d06
Sylivester Mberenge <s.schmitzerx@gmail.com>
2023-04-02 08:58:35 +0300

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -15,11 +15,14 @@ int is_palindrome(char *s)
 	int count, length;
 
 	count = 0;
+ /* Calculating length of the string - palindrome candidate */
 	length = string_length(s);
 
+ /* incase string is empty */
 	if (!(*s))
 		return (1);
 
+  /* Function to check first and last characters */
 	return (test_pal(s, length, count));
 }
 
@@ -55,10 +58,14 @@ int string_length(char *s)
 
 int test_pal(char *s, int length, int count)
 {
+ /* Checking if the string is NULL */
 	if (s[count] == s[length / 2])
 		return (1);
 
+  /* Checking if two characters are identical */
 	if (s[count] == s[length - count - 1])
+
+  /* Continuing to check more pairs using Recursion */
 		return (test_pal(s, length, count + 1));
 
 	return (0);
