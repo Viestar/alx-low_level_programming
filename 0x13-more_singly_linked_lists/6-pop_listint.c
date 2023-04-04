b7b1d87a022e670a33cdf3fc068d20d0baa770d3
Viestar <m.silverbuckson@gmail.com>
2023-04-04 03:00:42 +0300

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -4,22 +4,21 @@
  * pop_listint - Deletes head node
  * @head: list
  * Return: node value or 0
-*/
+ */
 
 int pop_listint(listint_t **head)
 {
-    int node_value;
-    listint_t *new_list;
+	int node_value;
+	listint_t *new_list;
 
-    if (*head == NULL)
-        return (0);
+	if (*head == NULL)
+		return (0);
 
-    new_list = *head;
-    node_value = (*(*head)).n;
-    *head = (*(*head)).next;
+	new_list = *head;
+	node_value = (*(*head)).n;
+	*head = (*(*head)).next;
 
+	free(new_list);
 
-    free(new_list);
-
-    return (node_value);
+	return (node_value);
 }
