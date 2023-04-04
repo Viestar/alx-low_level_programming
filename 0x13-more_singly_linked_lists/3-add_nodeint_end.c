f33d05a993b27698a900e3359e84cbe263da830d
Viestar <m.silverbuckson@gmail.com>
2023-04-04 16:57:23 +0300

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,6 +11,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	/* Variable to store the new list */
 	listint_t *new_list;
+	listint_t *old_one;
 
 	/* allocating memory for the new list */
 	new_list = malloc(sizeof(listint_t));
@@ -18,25 +19,30 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	/* Checking if any memory was allocated */
 	if (new_list == NULL)
 		return (NULL);
-	/* */
+
+	/* Adding node n to new the new list */
 	(*new_list).n = n;
 
-	/* */
+	/* Pointing the next node to NULL */
 	(*new_list).next = NULL;
 
-	/**/
+	/*incase the passed list was empty */
 	if (*head == NULL)
 	{
-		/* Incase *head was passed empty, only new node will be added */
+		/* Then new_list will just replace the older one */
 		*head = new_list;
 	}
 	else
 	{
-		while ((*(*head)).next != NULL)
-			*head = (*(*head)).next;
+		/* Cloning the list for ammendments*/
+		old_one = *head;
 
-		/* Adding n to the end of the old list */
-		(*(*head)).next = new_list;
+		/* Looping through the list until the node */
+		while ((*old_one).next != NULL)
+			old_one = (*old_one).next;
+
+		/* Adding n by pointing the last node to the new list */
+		(*old_one).next = new_list;
 	}
 	/* */
 	return (*head);
