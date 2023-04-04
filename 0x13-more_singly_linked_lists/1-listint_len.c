4eacf8726e4270f264d07d2e864526b10d63c91f
Viestar <m.silverbuckson@gmail.com>
2023-04-04 03:12:26 +0300

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,21 +3,21 @@
 /**
  * listint_len - Prints number of elements
  * @h: pointer to the head of the list
-*/
+ * Return: number of elements printed
+ */
 
 size_t listint_len(const listint_t *h)
 {
-    /* Variable of size_t to store number of elements */
-    size_t elements = 0;
+	/* Variable of size_t to store number of elements */
+	size_t elements = 0;
 
-    while (h)
-    {
-        /* Calculating number of elements */
-        elements++;
+	while (h)
+	{
+		/* Calculating number of elements */
+		elements++;
 
-        /* Pointing to the next head */
-        h = (*h).next;
-
-    }
-    return (elements);
+		/* Pointing to the next head */
+		h = (*h).next;
+	}
+	return (elements);
 }
