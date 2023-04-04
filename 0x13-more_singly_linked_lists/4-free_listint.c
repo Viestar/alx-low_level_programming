71194b9a21f8d044843fd24c034e3fbfd56a9c2f
Viestar <m.silverbuckson@gmail.com>
2023-04-04 21:08:47 +0300

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -4,22 +4,21 @@
  * free_listint - Frees a list
  * @head: list to be freed
  * Return: Void
-*/
+ */
 
 void free_listint(listint_t *head)
 {
-    listint_t *empty_list;
+	listint_t *empty_list;
 
-    while (head)
-    {
-        /* */
-        empty_list = (*head).next;
+	while (head)
+	{
+		/* */
+		empty_list = (*head).next;
 
-        /* */
-        free(head);
+		/* */
+		free(head);
 
-        /* */
-        head = empty_list;
-    }
-
+		/* */
+		head = empty_list;
+	}
 }
