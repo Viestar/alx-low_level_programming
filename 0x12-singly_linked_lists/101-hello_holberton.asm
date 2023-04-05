d5f555a80278dc60e4801b9186968c8675b48d29
Viestar <m.silverbuckson@gmail.com>
2023-04-05 14:23:32 +0300

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,22 +1,27 @@
-/* Need to do more research on this solution */
+; File: 101-hello_holberton.asm
+; Author: Sylivester Mberenge
+; Desc: system of 64-bit printing a string and a newline
+
+
 extern printf
 
 section .text
-   global main
+	global main
 
 main:
-   push rbp
+	push rbp
 
-   mov rdi,fmt
-   mov rsi,msg
-   mov rax,0
-   call printf
+	mov rdi,fmt
+	mov rsi,msg
+	mov rax,0
+	call printf
 
-   pop rbp
+	pop rbp
 
-   mov rax,0
-   ret
+	mov rax,0
+	ret
 
 section .data
-   msg: db "Hello, Holberton", 0
-   fmt: db "%s", 10, 0
+	msg: db "Hello, Holberton", 0
+	fmt: db "%s", 10, 0
+	
