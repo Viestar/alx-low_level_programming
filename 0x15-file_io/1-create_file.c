16a247f714e1e7f38679ccc812f813e2c3a39797
Viestar <m.silverbuckson@gmail.com>
2023-04-08 16:19:21 +0300

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file.
+ * @filename: file to be created
+ * @text_content: string to be written.
+ * Return: 1 for success
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	/* Declarations and Initialisations */
+	int string_len = 0;
+	int fildes, filwrt;
+
+	/* Finding length of the passed string */
+	while (text_content[string_len])
+		string_len++;
+
+	/* openning/creating the file with permissions */
+	fildes = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+
+	/* Writing contents to the file */
+	filwrt = write(fildes, text_content, string_len);
+
+	/* Incase no files were written */
+	if (!fildes || !filwrt)
+		return (-1);
+
+	close(fildes);
+
+	/* Products */
+	return (1);
+}
