7fb36a0a6c5f1b730d67272e01c5d7822cd313a6
Viestar <m.silverbuckson@gmail.com>
2023-04-09 15:15:17 +0300

    main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 
 /* Prototypes*/
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
