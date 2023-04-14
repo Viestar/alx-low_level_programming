a191d4dae86582f55701fe552fa2a8b43428050b
Viestar <m.silverbuckson@gmail.com>
2023-04-14 20:45:56 +0300

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -3,29 +3,32 @@
 #include <unistd.h> /* POSIX-1 STDS 0-2*/
 #include <fcntl.h>	/* File Modes */
 #include <elf.h>	/* Elf64_Ehdr */
+#include <sys/types.h>
 
 /* Prototypes */
 void file_closer(int file);
 void elf_checker(unsigned char *e_ident);
+void os_abi_printer(unsigned char *e_ident);
+void type_printer(unsigned char *e_ident, unsigned int e_type);
+void entry_printer(unsigned char *e_ident, unsigned long int e_entry);
 void magic_printer(unsigned char *e_ident);
 void class_printer(unsigned char *e_ident);
 void data_printer(unsigned char *e_ident);
 void version_printer(unsigned char *e_ident);
-void os_abi_printer(unsigned char *e_ident);
-void type_printer(unsigned int e_type, unsigned char *e_ident);
-void entry_printer(unsigned long int e_entry, unsigned char *e_ident);
+
 /**
- * main - Displays ELF elf_head infomation at the start of an ELF file
+ * main - Displays ELF_header infomation at the start of an ELF file.
  * @argc: number of arguments
  * @argv: Array of arguments.
  * Return: 0 for sucess
  */
 
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	Elf64_Ehdr *elf_head;
 
 	int filopn, filrd;
+	(void)argc;
 
 	/* Reading elf file passed as the second argument */
 	filopn = open(argv[1], O_RDONLY);
@@ -36,25 +39,26 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		exit(98);
 	}
 
+	/* allocating memory to the buffer*/
 	elf_head = malloc(sizeof(Elf64_Ehdr));
-	if (elf_head == NULL)
+	if (!elf_head)
 	{
-		file_closer(filopn);
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+		file_closer(filopn);
 		exit(98);
 	}
 
-	/* Using the descriptor too read into elf_head_buffer*/
+	/* Using the descriptor to read into elf_head_buffer*/
 	filrd = read(filopn, elf_head, sizeof(Elf64_Ehdr));
 	if (filrd == -1)
 	{
-		free(elf_head);
-		file_closer(filopn); /* Own closing function */
 		dprintf(STDERR_FILENO, "Error: File %s not found\n", argv[1]);
+		file_closer(filopn); /* Own closing function */
+		free(elf_head);
 		exit(98);
 	}
 
-	elf_checker((*elf_head).e_ident); /* Checking if elf or not*/
+	elf_checker((*elf_head).e_ident); /* ELF checker */
 
 	printf("ELF Header:\n");
 
@@ -65,23 +69,44 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	os_abi_printer((*elf_head).e_ident);  /* OS/ABI */
 
 	/*Printing ABI Version */
-	printf(" ABI Version:  %d", (*elf_head).e_ident[EI_ABIVERSION]);
+	printf("  ABI Version:                       %d",
+		   (*elf_head).e_ident[EI_ABIVERSION]);
 
-	type_printer((*elf_head).e_type, (*elf_head).e_ident);	 /* Type */
-	entry_printer((*elf_head).e_entry, (*elf_head).e_ident); /* Entry point */
+	type_printer((*elf_head).e_ident, (*elf_head).e_type);	 /* Type */
+	entry_printer((*elf_head).e_ident, (*elf_head).e_entry); /* Entry point */
 
 	free(elf_head);
 	file_closer(filopn);
 	return (0);
 }
+
+/**
+ * file_closer - closes the passed file
+ * @file: file to be closed
+ * Return: void
+ */
+
+void file_closer(int file)
+{
+	/* Store the closing descriptor */
+	int closer;
+
+	closer = close(file);
+	if (closer == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: File desc. %d can't be closed", file);
+		exit(98);
+	}
+}
+
 /**
  * entry_printer - Prints ELF Header entry point
  * @e_entry: Entry point address
  * @e_ident: pointer to the array.
  */
-void entry_printer(unsigned long int e_entry, unsigned char *e_ident)
+void entry_printer(unsigned char *e_ident, unsigned long int e_entry)
 {
-	printf(" Entry point address: ");
+	printf("  Entry point address:               ");
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 	{
 		e_entry = ((e_entry << 8) & 0xFF00FF00) |
@@ -102,11 +127,11 @@ void entry_printer(unsigned long int e_entry, unsigned char *e_ident)
  * @e_ident: pointer to the array
  */
 
-void type_printer(unsigned int e_type, unsigned char *e_ident)
+void type_printer(unsigned char *e_ident, unsigned int e_type)
 {
 	if (e_ident[EI_DATA == ELFDATA2MSB])
 		e_type = e_type >> 8;
-	printf(" Type: ");
+	printf("  Type:                              ");
 
 	if (e_type == ET_REL)
 		printf("REL (Relocatable file)\n");
@@ -128,7 +153,7 @@ void type_printer(unsigned int e_type, unsigned char *e_ident)
  */
 void os_abi_printer(unsigned char *e_ident)
 {
-	printf(" OS/ABI");
+	printf("  OS/ABI:                            ");
 	switch (e_ident[EI_OSABI])
 	{
 	case ELFOSABI_HPUX:
@@ -172,7 +197,7 @@ void os_abi_printer(unsigned char *e_ident)
  */
 void version_printer(unsigned char *e_ident)
 {
-	printf(" Version: %d", e_ident[EI_VERSION]);
+	printf("  Version:                           %d", e_ident[EI_VERSION]);
 	if (e_ident[EI_VERSION == EV_CURRENT])
 		printf("(current)\n");
 	else
@@ -185,7 +210,7 @@ void version_printer(unsigned char *e_ident)
  */
 void data_printer(unsigned char *e_ident)
 {
-	printf(" Data: ");
+	printf("  Data:                              ");
 	if (e_ident[EI_DATA] == ELFDATANONE)
 		printf("none\n");
 	else if (e_ident[EI_DATA] == ELFDATA2MSB)
@@ -200,9 +225,10 @@ void data_printer(unsigned char *e_ident)
  * class_printer - prints ELF header class
  * @e_ident: Pointer to the class.
  */
+
 void class_printer(unsigned char *e_ident)
 {
-	printf("Class: ");
+	printf("  Class:                             ");
 	if (e_ident[EI_CLASS] == ELFCLASSNONE)
 		printf("none\n");
 	else if (e_ident[EI_CLASS] == ELFCLASS32)
@@ -224,11 +250,11 @@ void magic_printer(unsigned char *e_ident)
 
 	count = 0;
 
-	printf("Magic");
+	printf("  Magic:   ");
 	while (count < EI_NIDENT)
 	{
 		printf("%02x", e_ident[count]);
-		if (count == EI_NIDENT - 1)
+		if (count == (EI_NIDENT - 1))
 			printf("\n");
 		else
 			printf(" ");
@@ -257,23 +283,5 @@ void elf_checker(unsigned char *e_ident)
 			exit(98);
 		}
 		count++;
-	}
-}
-
-/**
- * file_closer - closes the passed file
- * @file: file to be closed
- * Return: void
- */
-
-void file_closer(int file)
-{
-	int closer;
-
-	closer = close(file);
-	if (closer == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: File %d can't be closed", file);
-		exit(98);
 	}
 }
