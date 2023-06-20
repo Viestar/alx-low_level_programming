a249c0b710ab2ec804c188e02e0e67cf9e55d10c
Viestar <m.silverbuckson@gmail.com>
2023-06-20 23:42:28 +0300

    betty hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -48,7 +48,7 @@ void hash_table_delete(hash_table_t *ht);
  * The key is unique in the HashTable
  * @value: The value corresponding to a key
  * @next: A pointer to the next node of the List
- * @sprev: A pointer to the previous                                                                                                                                                                                                                                                                                                                          element of the sorted linked list
+ * @sprev: A pointer to the previous
  * @snext: A pointer to the next element of the sorted linked list
  */
 
