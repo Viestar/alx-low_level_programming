c32b307f72c86cc2a47de5b0464036d0d566008c
Viestar <m.silverbuckson@gmail.com>
2023-09-12 12:48:40 +0300

    betty styled

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,5 +1,5 @@
 #ifndef SEARCH_ALGO_H
-#define SEARCH_ALGO_Hint linear_search(int *array, size_t size, int value);
+#define SEARCH_ALGO_H
 #include <stdio.h>  /* printf */
 #include <stdlib.h> /* size_t */
 
