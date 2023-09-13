676a460360fafff6a19e734f82983768d7037f1b
Viestar <m.silverbuckson@gmail.com>
2023-09-13 08:12:24 +0300

    Used while loop instead of recursion

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,57 +1,41 @@
 #include "search_algos.h"
 
-int search_recursion(int *array, size_t top, size_t index, int value);
-
 /**
  * binary_search - finds a given item by divide and conquer.
  * @array: List of items to be searched.
  * @size: size of the array to be searched.
  * @value: item to be searched.
- * Return: index of the item or -1 incase of failure.
+ * Return: Index of the item or -1 in case of failure.
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t top = size - 1;
-	size_t index = 0;
+	size_t below = 0;
+	size_t index, top = size - 1;
+	size_t middle;
 
-	if (!array)
+	if (array == NULL)
 		return (-1);
-	return (search_recursion(array, top, index, value));
-}
 
-/**
- * search_recursion - searches for a value in a sorted array
- * @array: pointer to the first element of the array
- * @top: number of elements in array
- * @index: keeps track of the index
- * @value: the value to search for
- * Return: if value is not present or NULL return -1
- */
-int search_recursion(int *array, size_t top, size_t index, int value)
-{
-	int mid_index;
-
-	size_t local_index = index;
-
-	if (top < index)
-		return (-1);
-	printf("Searching in array: ");
-
-	for (; local_index <= top; local_index++)
+	while (below <= top)
 	{
-		if (local_index == top)
-		{
-			printf("%d", array[local_index]);
-			break;
-		}
-		printf("%d, ", array[local_index]);
-	}
-	printf("\n");
+		middle = (below + top) / 2;
 
-	mid_index = (index + (top - index)) / 2;
-	if (array[mid_index] == value)
-		return (mid_index);
-	else if (array[mid_index] > value)
-		return (search_recursion(array, index, mid_index - 1, value));
-	return (search_recursion(array, mid_index + 1, top, value));
+		printf("Searching in array: ");
+		for (index = below; index <= top; index++)
+		{
+			printf("%d", array[index]);
+			if (index < top)
+				printf(", ");
+		}
+		printf("\n");
+
+		if (array[middle] == value)
+			return (middle);
+		else if (array[middle] < value)
+			below = middle + 1;
+		else
+			top = middle - 1;
+	}
+
+	return (-1); /* Value not found */
 }
