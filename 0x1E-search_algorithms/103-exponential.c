c39be849a03822e5e983633c64b6aff03aa72308
Viestar <m.silverbuckson@gmail.com>
2023-09-14 00:53:21 +0300

    103-exponential

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,22 +1,31 @@
 #include "search_algos.h"
-#include <math.h>
 #include <stdio.h>
 
 /**
- * binary_search - Searches for a value in a sorted array using Binary search.
+ * binary_search_exp - Searches for a value i
+ * n a sorted array using Binary search.
  * @array: Pointer to the first element of the array.
  * @low: The lower bound of the search range.
  * @high: The upper bound of the search range.
  * @value: The value to search for.
- * Return: The index of the value in the array or -1 if not found.
+ * Return: The index of the value in the array or (-1) if not found.
  */
 int binary_search_exp(int *array, int low, int high, int value)
 {
+	int i;
+
 	while (low <= high)
 	{
 		int mid = low + (high - low) / 2;
 
-		printf("Value checked array[%d] = [%d]\n", mid, array[mid]);
+		printf("Searching in array: ");
+		for (i = low; i <= high; i++)
+		{
+			if (i > low)
+				printf(", ");
+			printf("%d", array[i]);
+		}
+		printf("\n");
 
 		if (array[mid] == value)
 			return (mid);
@@ -31,19 +40,19 @@ int binary_search_exp(int *array, int low, int high, int value)
 }
 
 /**
- * exponential_search - Searches for a value in a sorte
- * d array using Exponential search.
+ * exponential_search - Searches for a value in a
+ * sorted array using Exponential search.
  * @array: Pointer to the first element of the array.
  * @size: Number of elements in the array.
  * @value: The value to search for.
- * Return: The index of the value in the array or -1 if not found.
+ * Return: The index of the value in the array or (-1) if not found.
  */
 int exponential_search(int *array, size_t size, int value)
 {
-	int high;
+	int high, low, bound;
 
-	int bound = 1;
-	int low = bound / 2;
+	if (array == NULL || size == 0)
+		return (-1);
 
 	if (array[0] == value)
 	{
@@ -51,12 +60,14 @@ int exponential_search(int *array, size_t size, int value)
 		return (0);
 	}
 
+	bound = 1;
 	while (bound < (int)size && array[bound] < value)
 	{
 		printf("Value checked array[%d] = [%d]\n", bound, array[bound]);
 		bound *= 2;
 	}
 
+	low = bound / 2;
 	high = (bound < (int)size) ? bound : (int)size - 1;
 
 	return (binary_search_exp(array, low, high, value));
